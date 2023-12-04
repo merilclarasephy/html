ed5d228e6a58c9a5894aa3a8d0baa0920756ad6e
merilclarasephy <merilclarasephy>
2023-12-04 10:35:57 +0500

    first commit

---
+++ oopsdemo/oops.py
@@ -0,0 +1,22 @@
+class student:
+    def __init__(self,name,designation):
+        print("student details")
+        self.name = name
+        self.designation = designation
+        print(self.name+"  "+self.designation)
+s=student("Anu", "Engineer")
+
+
+
+
+
+# class informations:
+#     def printdetails(self,name,designation):
+#
+#         print(self.name)
+#         print(self.designation)
+
+# i = informations()
+# i.printdetails("sarath","leader")
+
+
